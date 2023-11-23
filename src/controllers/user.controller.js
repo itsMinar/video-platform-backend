6164156e8352557cf3445e88e72bc72f42e10cfb
Md. Minar Munshi <minarmunshi46@gmail.com>
2023-11-23 23:31:42 +0600

    Fix all Register Controller Bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -26,10 +26,18 @@ const registerUser = asyncHandler(async (req, res) => {
 
   // check for images, check for avatar
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req?.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
-    throw new ApiError(400, 'Local Avatar file is required');
+    throw new ApiError(400, 'Avatar file is required');
   }
 
   // upload them to cloudinary, avatar
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,7 +17,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     });
 
     // file has been uploaded successfully
-    console.log('File is uploaded on Cloudinary', response.url);
+    fs.unlinkSync(localFilePath);
 
     return response;
   } catch (error) {
