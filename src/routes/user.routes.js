f7f25adafee02fc68738134ed658fb50153760c6
Md. Minar Munshi <minarmunshi46@gmail.com>
2023-12-25 22:13:42 +0600

    Add new Routes for User Controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ const {
   loginUser,
   logoutUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } = require('../controllers/user.controller.js');
 const { upload } = require('../middlewares/multer.middleware.js');
 const { verifyJWT } = require('../middlewares/auth.middleware.js');
@@ -29,5 +36,17 @@ router.route('/login').post(loginUser);
 // secured routes
 router.route('/refresh-token').post(refreshAccessToken);
 router.route('/logout').post(verifyJWT, logoutUser);
+router.route('/change-password').post(verifyJWT, changeCurrentPassword);
+router.route('/current-user').get(verifyJWT, getCurrentUser);
+router.route('/update-account').patch(verifyJWT, updateAccountDetails);
+router
+  .route('/avatar')
+  .patch(verifyJWT, upload.single('avatar'), updateUserAvatar);
+router
+  .route('/cover-image')
+  .patch(verifyJWT, upload.single('coverImage'), updateUserCoverImage);
+
+router.route('/c/:username').get(verifyJWT, getUserChannelProfile);
+router.route('/history').get(verifyJWT, getWatchHistory);
 
 module.exports = router;
