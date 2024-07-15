17dd8060000c99615bf5b28030e5004a14d9b3d0
Md. Minar Munshi <minarmunshi46@gmail.com>
2024-07-15 18:26:30 +0600

    Add docker compose file

--- src/db/index.js
+++ src/db/index.js
@@ -3,9 +3,9 @@ const { DB_NAME } = require('../constants');
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
-    );
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: DB_NAME,
+    });
 
     console.log(
       `\nMongoDB Connected!! DB HOST: ${connectionInstance.connection.host} 📄`
