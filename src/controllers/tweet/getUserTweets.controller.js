811258cf403ebbfc8f6538a3dc1839e7abb1cd5f
Md. Minar Munshi <minarmunshi46@gmail.com>
2024-07-26 15:04:42 +0600

    Do some fixes

--- src/controllers/tweet/getUserTweets.controller.js
+++ src/controllers/tweet/getUserTweets.controller.js
@@ -2,22 +2,30 @@ const { asyncHandler } = require('../../utils/asyncHandler');
 const { ApiResponse } = require('../../utils/ApiResponse');
 const { ApiError } = require('../../utils/ApiError');
 const { isValidObjectId } = require('mongoose');
+const { User } = require('../../models/user.model');
+const { Tweet } = require('../../models/tweet.model');
 
 const getUserTweets = asyncHandler(async (req, res) => {
-  // TODO: will do this
-  const { channelId } = req.params;
+  // TODO: aro onek kaj baki ache, bivinno query add korte hobe
+  const { userId } = req.params;
 
-  // check valid channelId
-  if (!isValidObjectId(channelId)) {
-    throw new ApiError(404, 'Invalid Channel ID');
+  // check valid userId
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(404, 'Invalid User ID');
   }
+
+  const user = await User.findById(userId).select('username');
+
+  if (!user) {
+    throw new ApiError(404, 'User not found!');
+  }
+
+  const allTweets = await Tweet.find({ owner: userId });
 
   // return response
   return res
     .status(200)
-    .json(
-      new ApiResponse(200, null, 'Subscribed Channel Info Fetched Successfully')
-    );
+    .json(new ApiResponse(200, allTweets, 'All Tweets Fetched Successfully'));
 });
 
 module.exports = getUserTweets;
